b9862a4e3bd2fbce3f54a2c5ad8b80fd4cee1e9b
Jacob Handwerger <jstudyeth@gmail.com>
2023-09-11 11:19:15 -0400

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,51 @@
+// import { useState, useEffect } from "react";
+// import { Squash as Hamburger } from 'hamburger-react'
+
+// export default function Navbar() {
+//   const [ isScrolled, setIsScrolled ] = useState(false);
+//   const [ isOpen, setIsOpen ] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 10) {
+//         setIsScrolled(true);
+//       } else {
+//         setIsScrolled(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+//   return (
+//     <nav className={isScrolled ? "scrolled" : ""}>
+//       <div className="navbar-container">
+//         <div className="logo-container">
+//           <a href="/">
+//             <img className="logo-img-navbar" src="./logo.png" alt="Logo"></img>
+//           </a>
+//         </div>
+//         <div className="nav-links">
+//           <a href="/about">About</a>
+//           <a href="/services">Services</a>
+//           <a href="/contact">Contact Us</a>
+//         </div>
+//       </div>
+//       {/* <Hamburger toggled={isOpen} toggle={setIsOpen} /> */}
+//     </nav>
+//   );
+// }
+
+
 import { useState, useEffect } from "react";
+import { Squash as Hamburger } from "hamburger-react";
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -11,19 +55,30 @@ export default function Navbar() {
         setIsScrolled(false);
       }
     };
-
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <nav className={isScrolled ? "scrolled" : ""}>
-      <a href="/"><img className="logo-img-navbar" src="./logo.png"></img></a>
-      <a href="/about">About</a>
-      <a href="/services">Services</a>
-      <a href="/contact">Contact Us</a>
+      <div className="navbar-container">
+        <div className="logo-container">
+          <a href="/">
+            <img className="logo-img-navbar" src="./logo.png" alt="Logo" />
+          </a>
+        </div>
+        <div className={`nav-links ${showMenu ? "show" : ""}`}>
+          <a href="/about" className="nav-link">About</a>
+          <a href="/services" className="nav-link">Services</a>
+          <a href="/contact" className="nav-link">Contact Us</a>
+        </div>
+        <div className={`hamburger ${showMenu ? "show" : ""}`}>
+          <Hamburger toggled={showMenu} toggle={setShowMenu} />
+        </div>
+      </div>
     </nav>
   );
 }
+
